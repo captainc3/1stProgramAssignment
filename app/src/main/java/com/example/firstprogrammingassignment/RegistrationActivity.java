308e5460562b425c3e60eb886f4852e85f30bf97
Steven Corrales <scorrales3@gatech.edu>
2019-09-02 16:47:28 -0400

    Initial commit, working on login, registration, and home screens

---
+++ app/src/main/java/com/example/firstprogrammingassignment/RegistrationActivity.java
@@ -0,0 +1,26 @@
+package com.example.firstprogrammingassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+    EditText emailID, password;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailID = findViewById(R.id.)
+    }
+}
